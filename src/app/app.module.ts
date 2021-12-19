cf14bcaf548d614170c109584cc84b9becae7224
Maximo Huaman Allccahuaman <mrenato.huaman@vallegrande.edu.pe>
2021-12-19 18:18:09 -0500

    Listado productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetallesComponent } from './detalles/detalles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetallesComponent
   ],
   imports: [
     BrowserModule,
